28571238b574f364a91670b750fc158850bd2dfc
siddick <mebramsha@paypal.com>
2013-04-25 15:18:01 +0530

    Added ipn_valid? method to validate IPN message

--- spec/button_manager_spec.rb
+++ spec/button_manager_spec.rb
@@ -3,16 +3,20 @@ require 'spec_helper'
 describe "ButtonManager" do
 
   before :all do
-    @client = PayPal::SDK::ButtonManager::API.new
+    @api = PayPal::SDK::ButtonManager::API.new
   end
 
   describe "Services" do
     PayPal::SDK::ButtonManager::Services.instance_methods.select{|s| s =~ /^[A-Z]/ and s !~ /^Build/ }.each do |service_method|
       it "make empty request to #{service_method}" do
-        response = @client.send(service_method, {})
+        response = @api.send(service_method, {})
         response.ack.should_not be_nil
       end
     end
+  end
+
+  it "Validate ipn message" do
+    @api.ipn_valid?("Invalid").should be_false
   end
 
 end
